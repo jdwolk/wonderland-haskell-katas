dbed6605f8744d0e44dbda0d0fa97060027766e2
Jordaniel Wolk <jd@philosophie.is>
2016-06-11 17:13:31 -0700

    First AlphabetCipher implementation

--- src/Katas/AlphabetCipher.hs
+++ src/Katas/AlphabetCipher.hs
@@ -1,6 +1,99 @@
 module Katas.AlphabetCipher (
-  theThing
+  Headers(..)
+, Row(..)
+, Table(..)
+, TableDirection(..)
+, makeTable
+, makeSquareTable
+, sliceBy
+, findIn
+, encode
+, letters
+, substitutions
+, repeatedWord
+, invertedFindIn
+, decode
 ) where
 
-theThing :: String
-theThing = "hi"
+import           Data.Char       (chr)
+import           Data.List       (elemIndex)
+import qualified Data.Map.Strict as Map
+import           Data.Maybe      (fromJust)
+
+type Headers a = [a]
+type Row a = (a, [a])
+type Keyword = String
+type CipherText = String
+
+data Table a = Table {
+    headers :: Headers a
+  , rows    :: Map.Map a [a]
+  , columns :: Map.Map a [a]
+  } deriving (Show)
+
+data TableDirection = RowDir | ColumnDir
+
+makeTable :: Ord a => Headers a -> [Row a] -> Table a
+makeTable hs rs = Table {
+    headers = hs
+  , rows    = Map.fromList rs
+  , columns = Map.fromList $ map (rowOfHeader hs rs) hs
+  }
+
+makeRow :: a -> [a] -> Row a
+makeRow rowId rowVals = (rowId, rowVals)
+
+makeSquareTable :: Ord a => Headers a -> Table a
+makeSquareTable hs = makeTable hs rows
+  where rows = map (\ a -> (a, replicate (length hs) a)) hs
+
+rowOfHeader :: Eq a => Headers a -> [Row a] -> a -> Row a
+rowOfHeader hs rs h = (h, map (getAtIndex (indexOf h hs)) rs)
+
+getAtIndex :: Int -> Row a -> a
+getAtIndex el (_, els) = els !! el
+
+indexOf :: Eq h => h -> Headers h -> Int
+indexOf h hs = fromJust $ elemIndex h hs
+
+sliceBy :: Ord a => TableDirection -> Table a -> a -> Maybe [a]
+sliceBy RowDir t h = Map.lookup h $ rows t
+sliceBy ColumnDir t h = Map.lookup h $ columns t
+
+letters :: String
+letters = map chr [97..122]
+
+startingFrom :: Eq a => a -> [a] -> [a]
+startingFrom el els = drop n els ++ take n els
+  where n = indexOf el els
+
+substitutions :: Table Char
+substitutions = makeTable letters letterRows
+  where letterRows = map (\l -> makeRow l $ startingFrom l letters) letters
+
+repeatedWord :: String -> String
+repeatedWord word = concat $ repeat word
+
+findIn :: Ord a => Table a -> a -> a -> a
+findIn table row column = rowVals !! colIndex
+  where rowVals = fromJust $ sliceBy RowDir table row
+        colIndex = indexOf column (headers table)
+
+invertedFindIn :: Ord a => Table a -> a -> a -> a
+invertedFindIn table column header = fst $ tableRowsList !! index
+  where columnVals = fromJust $ sliceBy ColumnDir table header
+        index = indexOf column columnVals
+        tableRowsList = Map.toList $ rows table
+
+encodeWith :: (Table Char -> Char -> Char -> Char) -> Keyword -> String -> String
+encodeWith fn key text =
+  map (uncurry $ fn substitutions) zipped
+    where zipped = zip text repeatedKey
+          repeatedKey = take (length text) $ repeatedWord key
+
+encode :: Keyword -> String -> CipherText
+encode = encodeWith findIn
+
+decode :: Keyword -> CipherText -> String
+decode = encodeWith invertedFindIn
+
--- test/Katas/AlphabetCipherSpec.hs
+++ test/Katas/AlphabetCipherSpec.hs
@@ -2,11 +2,51 @@ module Katas.AlphabetCipherSpec (
   spec
 ) where
 
-import           Katas.AlphabetCipher
+import           Data.Maybe           (fromJust)
+import           Katas.AlphabetCipher (Headers (..), Table (..),
+                                       TableDirection (..), decode, encode,
+                                       findIn, makeSquareTable, makeTable,
+                                       sliceBy)
 import           Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "does it work" $ do
-    it "works!" $ do
-      theThing `shouldBe` "hi"
+  describe "makeTable" $ do
+    it "has the correct number of rows" $ do
+      let rs = [("A", ["a", "b"]),
+                ("B", ["b", "c"])
+               ]
+          table = makeTable ["A", "B"] rs
+      (length $ rows table) `shouldBe` length rs
+
+  describe "sliceBy" $ do
+    context "RowDir" $ do
+      it "returns the value in the given table at the given row" $ do
+        let rowA = ["a", "b", "c"]
+            rowB = ["b", "c", "d"]
+            table = makeTable ["A", "B", "C"] [
+                      ("A", rowA),
+                      ("B", rowB)
+                    ]
+        (fromJust $ sliceBy RowDir table "A") `shouldBe` rowA
+
+  describe "findIn" $ do
+    it "looks up the given row and column in the table" $ do
+      let rowA = ["a", "b", "c"]
+          rowB = ["b", "c", "d"]
+          table = makeTable ["A", "B", "C"] [
+                    ("A", rowA),
+                    ("B", rowB)
+                  ]
+      findIn table "B" "B" `shouldBe` "c"
+
+  describe "encode" $ do
+    context "with keyword 'scone'" $ do
+      it "encodes correctly" $ do
+        encode "scones" "meetmebythetree" `shouldBe` "egsgqwtahuiljgs"
+
+  describe "decode" $ do
+    it "inverts an encoded string" $ do
+      let cipherText = "meetmebythetree"
+          key = "scones"
+      decode key (encode key cipherText) `shouldBe` cipherText
